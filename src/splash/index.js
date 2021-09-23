7f99f741799725dd2eceb777835c61aa48c5868e
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-23 14:00:46 +0300

    feature/splash

--- src/splash/index.js
+++ src/splash/index.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {ActivityIndicator, SafeAreaView, Text, View} from 'react-native';
+import {colors} from '../utils/colors';
 
 export default class Splash extends Component {
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
-          <View style={{flex: 1}}>
-
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: colors.color_primary,
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}>
+          <Text
+            style={{fontSize: 30, color: colors.white, alignSelf: 'center'}}>
+            Kibanda Agent App
+          </Text>
+          <View>
+            <ActivityIndicator
+              size={24}
+              color={colors.white}
+              style={{marginTop: 20}}
+            />
           </View>
+        </View>
       </SafeAreaView>
     );
   }
---
+++ src/utils/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  color_primary: '#4ba63c',
+  color_secondary: '#0a7607',
+  white: '#ffffff'
+};
