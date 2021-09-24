0cba91ac90fc53efa8c11546af4e5fcae5f250be
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-24 08:58:03 +0300

    fix/dashboard

--- src/home/index.js
+++ src/home/index.js
@@ -1,95 +1,59 @@
 import React, {Component} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
 import {Card, FAB} from 'react-native-paper';
-import {colors} from '../utils/colors';
+import styles from './style';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: [],
+    };
   }
 
   render() {
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={{flex: 1}}>
-          <View
-            style={{
-              flexDirection: 'row',
-              height: 160,
-              marginTop: 10,
-              alignContent: 'center',
-              justifyContent: 'space-evenly',
-            }}>
-            <Card
-              style={{
-                width: 160,
-                justifyContent: 'center',
-                borderRadius: 10,
-                elevation: 4,
-              }}>
-              <View style={{justifyContent: 'center', marginTop: 60}}>
-                <Text
-                  style={{
-                    alignSelf: 'center',
-                    fontWeight: 'bold',
-                    fontSize: 20,
-                  }}>
-                  Total Orders
-                </Text>
-                <Text style={{alignSelf: 'center', fontSize: 18}}>100</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <View style={styles.content}>
+            <Card style={styles.cardView}>
+              <View style={styles.dashboardCard}>
+                <Text style={styles.totalOrderTxt}>Total Orders</Text>
+                <Text style={styles.countTxt}>100</Text>
               </View>
             </Card>
-            <Card
-              style={{
-                width: 160,
-                justifyContent: 'center',
-                borderRadius: 10,
-                elevation: 4,
-              }}>
-              <View style={{justifyContent: 'center', marginTop: 60}}>
-                <Text
-                  style={{
-                    alignSelf: 'center',
-                    fontWeight: 'bold',
-                    fontSize: 20,
-                  }}>
-                  Total Clients
-                </Text>
-                <Text style={{alignSelf: 'center', fontSize: 18}}>50</Text>
+            <Card style={styles.cardView}>
+              <View style={styles.dashboardCard}>
+                <Text style={styles.latestClientTxt}>Total Clients</Text>
+                <Text style={styles.countTxt}>50</Text>
               </View>
             </Card>
           </View>
 
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              margin: 10,
-            }}>
+          <View style={styles.latestOrderView}>
             <View>
-              <Text style={{fontSize: 18, marginLeft: 10}}>Latest Order</Text>
+              <Text style={styles.latestOrderText}>Latest Order</Text>
             </View>
             <View>
-              <Text
-                style={{
-                  textDecorationLine: 'underline',
-                  color: colors.blue,
-                  marginRight: 10,
-                }}>
-                View all
-              </Text>
+              <Text style={styles.viewAll}>View all</Text>
             </View>
           </View>
+            <FlatList data={this.state.data} renderItem={(item) => (
+                <View>
+
+                </View>
+            )}
+              ListEmptyComponent={() => (
+                  <View>
+                      <Text>Orders not available</Text>
+                  </View>
+              )}
+            />
           <FAB
             icon="plus"
             onPress={() => console.log('Pressed')}
-            style={{
-              position: 'absolute',
-              margin: 16,
-              right: 0,
-              bottom: 0,
-              backgroundColor: colors.color_primary
-            }}
+            style={styles.fab}
           />
         </View>
       </SafeAreaView>
---
+++ src/home/style.js
@@ -0,0 +1,59 @@
+import {colors} from '../utils/colors';
+
+export default {
+  container: {
+    flex: 1,
+  },
+  content: {
+    flexDirection: 'row',
+    height: 160,
+    marginTop: 10,
+    alignContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  cardView: {
+    width: 160,
+    justifyContent: 'center',
+    borderRadius: 10,
+    elevation: 4,
+  },
+  dashboardCard: {
+    justifyContent: 'center',
+    marginTop: 60,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    backgroundColor: colors.color_primary,
+  },
+  viewAll: {
+    textDecorationLine: 'underline',
+    color: colors.blue,
+    marginRight: 10,
+  },
+  latestOrderView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+  },
+  latestOrderText: {
+    fontSize: 18,
+    marginLeft: 10,
+  },
+  latestClientTxt: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  countTxt: {
+    alignSelf: 'center',
+    fontSize: 18,
+  },
+  totalOrderTxt: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+};
